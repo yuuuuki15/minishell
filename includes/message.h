b7010123c269cd72664f249587e0e8e75ba6d5fe
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2024-02-20 18:31:06 +0100

    [create] message header. We can add error message here.

---
+++ includes/message.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   message.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykawakit <ykawakit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 18:04:13 by ykawakit          #+#    #+#             */
+/*   Updated: 2024/02/20 18:22:56 by ykawakit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MESSAGE_H
+# define MESSAGE_H
+
+# define ERR_PIPE "\
+Error\nPipe error.\n"
+# define ERR_FORK "\
+Error\nFork error."
+# define ERR_MALLOC "\
+Error\nMalloc fails."
+
+#endif
