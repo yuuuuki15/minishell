073cd5b614dec060586ad1c426dd4dae05622660
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2024-02-20 14:32:43 +0100

    [update] Makefile (changed header and main.c for test)

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,11 +6,13 @@
 /*   By: ykawakit <ykawakit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 18:40:07 by ykawakit          #+#    #+#             */
-/*   Updated: 2024/02/19 18:40:38 by ykawakit         ###   ########.fr       */
+/*   Updated: 2024/02/20 14:28:56 by ykawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+#include "../libft/libft.h"
 
 #endif
