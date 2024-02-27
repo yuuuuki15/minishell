18866484dd457462e5b9d187140cf6a4ab09fb05
yuuuuki15 <rx86340616@gmail.com>
2024-02-27 18:26:38 +0100

    [feat] add and update env

--- src/env.c
+++ src/env.c
@@ -6,7 +6,7 @@
 /*   By: ykawakit <ykawakit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 17:38:21 by ykawakit          #+#    #+#             */
-/*   Updated: 2024/02/27 17:54:54 by ykawakit         ###   ########.fr       */
+/*   Updated: 2024/02/27 18:26:16 by ykawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,55 @@ t_env	*ft_get_env(char *name)
 }
 
 /**
- * take key and value, then register to environment
+ * @param char* key
+ * @param char* value
+ * @return int
+ *
+ * This function will take key and value and update the environment
+ * with the sam key name env.
 */
-void	ft_add_env(char *key, char *value)
+void	ft_update_env(char *key, char *value)
 {
-	t_env	*env;
+	t_env	*curr;
 
-	if (get_env())
+	curr = shell.env;
+	while (curr)
+	{
+		if (ft_strcmp(curr->key, key) == 0)
+			break ;
+		curr = curr->next;
+	}
+	free(curr->value);
+	curr->value = ft_strdup(value);
+}
+
+/**
+ * @param char* key
+ * @param char* value
+ * @return int
+ *
+ * take key and value, then register to environment.
+ * return 0 on success. return 1 on error.
+*/
+int	ft_add_env(char *key, char *value)
+{
+	t_env	*new;
+	t_env	*curr;
+
+	if (ft_get_env(key) == NULL)
+	{
+		ft_update_env(key, value);
+		return (0);
+	}
+	new = malloc(sieof(t_env));
+	if (new == NULL)
+		return (1);
+	new->key = ft_strdup(key);
+	new->value = ft_strdup(value);
+	new->next = NULL;
+	curr = shell.env;
+	while (curr->next != NULL)
+		curr = curr->next;
+	curr->next = new;
+	return (0);
 }
