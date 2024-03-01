d2b3de93d6ac9583b0aa216e5164219b72a84de8
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2024-03-01 16:02:37 +0100

    [update] handle three different signal

--- src/signal.c
+++ src/signal.c
@@ -6,22 +6,73 @@
 /*   By: ykawakit <ykawakit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:35:34 by ykawakit          #+#    #+#             */
-/*   Updated: 2024/03/01 15:36:51 by ykawakit         ###   ########.fr       */
+/*   Updated: 2024/03/01 16:02:22 by ykawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_handler(int sig)
+/**
+ * @param sig The signal number.
+ *
+ * Handles the SIGINT signal by printing a newline and the prompt.
+ * Linux: Ctrl + C
+ */
+static void	ft_handler(int sig)
 {
-	printf("hello %d\n", sig);
+	if (sig == SIGINT)
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		ft_putstr_fd(PROMPT, STDOUT_FILENO);
+	}
 }
 
+/**
+ * @param sig The signal number.
+ *
+ * Ignores the received signal.
+ * Linux: Ctrl + \
+ */
+static void	ft_ignore_signal(int sig)
+{
+	(void)sig;
+}
+
+/**
+ * @param sig The signal number.
+ *
+ * Exits the program when SIGKILL is received.
+ * Linux: Ctrl + D
+ * Mac: Ctrl + Z
+ */
+static void	ft_exit_program(int sig)
+{
+	if (sig == SIGKILL)
+	{
+		ft_putstr_fd("exit minishell\n", STDOUT_FILENO);
+		exit(0);
+	}
+}
+
+/**
+ * Sets up signal handlers for SIGINT, SIGQUIT, and SIGKILL.
+ */
 void	ft_signal_manager(void)
 {
-	struct	sigaction	sa;
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+	struct sigaction	sa_exit;
 
-	sa.sa_handler = &ft_handler;
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGTSTP, &sa, NULL);
+	sa_int.sa_handler = ft_handler;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa_int, NULL);
+	sa_quit.sa_handler = ft_ignore_signal;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = SA_RESTART;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+	sa_exit.sa_handler = ft_exit_program;
+	sigemptyset(&sa_exit.sa_mask);
+	sa_exit.sa_flags = SA_RESTART;
+	sigaction(SIGKILL, &sa_exit, NULL);
 }
