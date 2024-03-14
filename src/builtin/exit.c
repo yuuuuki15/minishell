37a689ac5d2698cd69f102ea6d704cbc43a42f4b
mevonuk <mevonuk@gmail.com>
2024-03-14 16:31:48 +0100

    all normed, leak still in heredoc

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -12,12 +12,31 @@
 
 #include "minishell.h"
 
-int	ft_exit(t_execcmd *cmd, t_shell *shell)
+// set exit tag if one argument, raise error if non-numeric
+void	exit_one_arg(t_execcmd *cmd, t_shell *shell)
 {
 	int	i;
-	int	j;
 
 	i = -1;
+	while (cmd->argv[1][++i])
+	{
+		if (!ft_isdigit(cmd->argv[1][i]))
+		{
+			ft_putendl_fd("minishell: exit: numeric argument required",
+				STDERR_FILENO);
+			shell->exit_prog = 2;
+			exit_shell(shell);
+		}
+	}
+	shell->exit_prog = ft_atoi(cmd->argv[1]);
+}
+
+// exit program
+// if too many arguments raise error
+int	ft_exit(t_execcmd *cmd, t_shell *shell)
+{
+	int	j;
+
 	j = 0;
 	while (cmd->argv[j] != NULL)
 	{
@@ -27,18 +46,7 @@ int	ft_exit(t_execcmd *cmd, t_shell *shell)
 			return (1);
 		}
 		else if (j == 1 && cmd->argv[j] != NULL)
-		{
-			while (cmd->argv[1][++i])
-			{
-				if (!ft_isdigit(cmd->argv[1][i]))
-				{
-					ft_putendl_fd("minishell: exit: numeric argument required", STDERR_FILENO);
-					shell->exit_prog = 2;
-					exit_shell(shell);
-				}
-			}
-		shell->exit_prog = ft_atoi(cmd->argv[1]);
-		}
+			exit_one_arg(cmd, shell);
 		j++;
 	}
 	exit_shell(shell);
