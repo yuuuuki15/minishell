237d89e584f1313e7b708a4b7653c153b732048b
yuuuuki15 <rx86340616@gmail.com>
2024-03-19 08:57:00 +0100

    [fix] bash takes any argument for pwd

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ykawakit <ykawakit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 15:27:13 by ykawakit          #+#    #+#             */
-/*   Updated: 2024/03/12 16:30:44 by ykawakit         ###   ########.fr       */
+/*   Updated: 2024/03/19 08:55:21 by ykawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,9 @@ int	ft_pwd(t_execcmd *cmd, t_shell *shell)
 
 	(void)cmd;
 	env = ft_get_env("PWD", shell);
-	if (cmd->argv[1] != NULL)
+	if (cmd->argv[1] != NULL && cmd->argv[1][0] == '-')
 	{
-		if (cmd->argv[1][0] == '-')
-			ft_putendl_fd(ERR_OPTION_NOT_SUPPORTED, STDERR_FILENO);
-		else
-			ft_putendl_fd(ERR_TOO_MANY_ARGUMENT, STDERR_FILENO);
+		ft_putendl_fd(ERR_OPTION_NOT_SUPPORTED, STDERR_FILENO);
 		return (1);
 	}
 	if (env == NULL)
