d81414a23d7b401673574594721bc0c2e13060a7
yuuuuki15 <rx86340616@gmail.com>
2024-03-19 11:22:50 +0100

    [fix] valgrind error

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,36 +6,51 @@
 /*   By: ykawakit <ykawakit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 19:21:00 by ykawakit          #+#    #+#             */
-/*   Updated: 2024/03/19 10:03:24 by ykawakit         ###   ########.fr       */
+/*   Updated: 2024/03/19 11:22:34 by ykawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// exit program
-// if too many arguments raise error
-int	ft_exit(t_execcmd *cmd, t_shell *shell)
+// set exit tag if one argument, raise error if non-numeric
+void	exit_one_arg(t_execcmd *cmd, t_shell *shell)
 {
 	int	i;
 
 	i = -1;
-	if (cmd->argv[1][0] == '+' || cmd->argv[1][0] == '-')
-		i++;
 	while (cmd->argv[1][++i])
 	{
+		if (cmd->argv[1][i] == '+' || cmd->argv[1][i] == '-')
+			i++;
 		if (!ft_isdigit(cmd->argv[1][i]))
 		{
-			ft_putendl_fd(ERR_EXIT_NUMERIC_REQUIRED, STDERR_FILENO);
+			ft_putendl_fd("minishell: exit: numeric argument required",
+				STDERR_FILENO);
 			shell->exit_prog = 2;
 			exit_shell(shell);
 		}
 	}
-	if (cmd->argv[2] != NULL)
-	{
-		ft_putendl_fd(ERR_EXIT_TOO_MANY_ARGUMENTS, STDERR_FILENO);
-		return (1);
-	}
 	shell->exit_prog = ft_atoi(cmd->argv[1]);
+}
+
+// exit program
+// if too many arguments raise error
+int	ft_exit(t_execcmd *cmd, t_shell *shell)
+{
+	int	j;
+
+	j = 0;
+	while (cmd->argv[j] != NULL)
+	{
+		if (j == 2 && cmd->argv[j] != NULL)
+		{
+			ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+			return (1);
+		}
+		else if (j == 1 && cmd->argv[j] != NULL)
+			exit_one_arg(cmd, shell);
+		j++;
+	}
 	exit_shell(shell);
-	return (1);
+	return (0);
 }
