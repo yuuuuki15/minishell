a219d6c110bf00ea0e2f3386e5b54665063281e9
mevonuk <mevonuk@gmail.com>
2024-03-21 15:28:50 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
 if [ $1 == "log" ]
 then
-	valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --log-file=minishell.log --track-fds=yes ./minishell
+	valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --log-file=minishell.log --track-fds=yes --trace-children=yes ./minishell
 else
-	valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --track-fds=yes ./minishell
+	valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --track-fds=yes --trace-children=yes ./minishell
 fi
