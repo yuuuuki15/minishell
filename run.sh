c2f3604bb54c9aa543a17629aa7aafc24a2c8e6e
mevonuk <mevonuk@gmail.com>
2024-03-26 09:57:57 +0100

    additional signal in heredoc

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ if [ $1 == "log" ]
 then
 	valgrind --leak-check=full --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --log-file=minishell.log --track-fds=yes --trace-children=yes ./minishell
 else
-	valgrind --leak-check=full --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --track-fds=yes --trace-children=yes ./minishell
+	valgrind --leak-check=full --show-reachable=yes --error-limit=no --suppressions=./valgrind-readline.supp --gen-suppressions=all --track-fds=yes --trace-children=yes ./minishell
 fi
